a8b144ca8b6b70cedad8bdaf59da1a5ac9a590ee
sarahmarkland <sarah.markland@icloud.com>
2023-07-20 12:25:34 -0500

    latin is dead

---
+++ api/0-gather_data_from_an_API.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import requests
+import sys
+
+
+def get_employee_tasks(employeeId):
+    """Get the tasks of an employee"""
+    url = "https://jsonplaceholder.typicode.com/"
+    url += "users/{}/todos".format(employeeId)
+    response = requests.get(url)
+    return response.json()
+
+
+def get_employee_name(employeeId):
+    """Get the name of an employee"""
+    url = "https://jsonplaceholder.typicode.com/"
+    url += "users/{}".format(employeeId)
+    response = requests.get(url)
+    return response.json().get("name")
+
+
+def get_completed_tasks(tasks):
+    """Get the completed tasks of an employee"""
+    completed_tasks = []
+    for task in tasks:
+        if task.get("completed"):
+            completed_tasks.append(task)
+    return completed_tasks
+
+
+def print_employee_tasks(employeeName, completedTasks, totalTasks):
+    """Print the tasks of an employee"""
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, len(completedTasks), totalTasks))
+    for task in completedTasks:
+        print("\t {}".format(task.get("title")))
+
+
+if __name__ == "__main__":
+    employeeId = sys.argv[1]
+    tasks = get_employee_tasks(employeeId)
+    employeeName = get_employee_name(employeeId)
+    completedTasks = get_completed_tasks(tasks)
+    print_employee_tasks(employeeName, completedTasks, len(tasks))
---
+++ api/README.md
@@ -0,0 +1,19 @@
+## General
+
+    What Bash scripting should not be used for
+### What is an API
+    An application program interface (API) is a set of routines, protocols, and tools for building software applications. Basically, an API specifies how software components should interact. Additionally, APIs are used when programming graphical user interface (GUI) components. A good API makes it easier to develop a program by providing all the building blocks. A programmer then puts the blocks together.
+
+    An API isn’t the same as the remote server — rather it is the part of the server that receives requests and sends responses.
+
+### What is a REST API
+    A REST API is a way for two computer systems to communicate using the HTTP technologies found in web browsers and servers.
+### What are microservices
+### What is the CSV format
+### What is the JSON format
+### Pythonic Package and module name style
+### Pythonic Class name style
+### Pythonic Variable name style
+### Pythonic Function name style
+### Pythonic Constant name style
+### Significance of CapWords or CamelCase in Python
